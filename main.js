7a8be5b82b66f67b86ac83462e20ff87b184ad21
David May <mayd2@rpi.edu>
2019-07-15 21:57:49 -0400

    Fully functioning

--- main.js
+++ main.js
@@ -3,15 +3,25 @@ const auth = require('./auth.json');
 
 /*
 Add bot:
-https://discordapp.com/oauth2/authorize?&client_id=555173602006138890&scope=bot&permissions=67648
+https://discordapp.com/oauth2/authorize?&client_id=600467162485751819&scope=bot&permissions=130112
 */
 
 const punctuation = ['.', '!', '?'];
 const contpunct = [',', '"', "'", ')', '}', ']'];
+const ouijachannels = ["599311652785225760", "599713645588512778"];
+
+var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+var regex = new RegExp(expression);
+
+let lastpunctID = 0;
 
 const bot = new Discord.Client({
     autorun: true,
-    token: auth.token,
+    token: auth.token
+});
+
+bot.on('ready', function(event) {
+    console.log('Logged in as %s - %s\n', bot.username, bot.id);
 });
 
 bot.setPresence({
@@ -20,96 +30,162 @@ bot.setPresence({
     }
 });
 
-bot.on('ready', function(event) {
-    //console.log('Logged in as %s - %s\n', bot.username, bot.id);
-});
-
 bot.on('message', async function(user, userID, channelID, message, evt) {
-    if(user.bot == true) {
-        if(channelID === 599311652785225760) {
+    //console.log(channelID);
+    if(userID == bot.id) {
+        if(ouijachannels.includes(channelID)) {
             bot.pinMessage({
                 channelID: channelID,
                 messageID: evt.d.id,
             });
-        }
-        return;
-    }
-    //user wants help: print useage and options
-    if(message.indexOf("!ouijabot") != -1) {
-        bot.sendMessage({
-            to: channelID,
-            message: "Ouijabot will automatically regulate a 'ouija' channel and post the resulting sentences in a 'wordofouija' channel."
-        });
-        return;
-    }
-    if(channelID === 599311652785225760) {
-        let resp = validate(userID, message);
-        if(resp === 0) { //punctated
-            //if punctuation we post a message in the ouija channel with a pin
-            //get history
-            let messagearray = [];
-            bot.getMessages({
-                channelID: 599311652785225760,
-                before: evt.d.id,
-                after: lastpunctID,
-                limit: 100 
-            }, err, messagearray);
-            messagearray.push(message);
             lastpunctID = evt.d.id;
-            //construct message
-            sentence = messagearray.join(' ');
-            //post
-            bot.sendMessage({
-                to: channelID,
-                message: sentence,
-                typing: true
-            });
-            return;
-        } else if(resp === -1) { //invalid
-            //delete message
+            if(message.length == 0) {
+                bot.deleteMessage({
+                    channelID: channelID,
+                    messageID: evt.d.id,
+                });
+            }
+        }
+        console.log("Bot posted in channel!, %s, %s", message, message.length);
+        return;
+    }
+    if(ouijachannels.includes(channelID)) {
+        console.log("read a message in a ouija channel: %s", message);
+        if(lastpunctID == 0) {
+            lastpunctID = evt.d.id;
+        }
+        //links
+        if(message.match(regex)) {
+            console.log("matched link regex");
             bot.deleteMessage({
                 channelID: channelID,
                 messageID: evt.d.id,
             });
             return;
+        }
+        //
+        let resp = validate(userID, message);
+        if(resp === 0) { //punctated
+            console.log("message was punctuation. printing full message:");
+            //if punctuation we post a message in the ouija channel with a pin
+            //get history
+            bot.getMessages({
+                channelID: channelID,
+                before: evt.d.id,
+                after: lastpunctID,
+                limit: 100 
+            }, function(err, response) {
+                let messagearray = [];
+                for (var i in response.reverse()) {
+                    messagearray.push(response[i]['content']);
+                }
+                //construct message
+                sentence = construct(messagearray);
+                console.log(sentence);
+                //post
+                bot.sendMessage({
+                    to: channelID,
+                    message: sentence
+                });
+            });
+            return;
+        } else if(resp === -1) { //invalid
+            console.log("invalid message, deleted");
+            //delete message
+            bot.deleteMessage({
+                channelID: channelID,
+                messageID: evt.d.id,
+            });
+            prevuser = prevuser2;
+            return;
         } else {
             return;
         }
     }
+    //user wants help: print useage and options
+    if(message.indexOf("!ouijabot") != -1) {
+        bot.sendMessage({
+            to: channelID,
+            message: "Ouijabot will automatically regulate the 'ouija' channel and post the resulting sentences."
+        });
+        return;
+    }
 });
 
+let prevuser2 = 0;
 let prevuser = 0;
 function validate(user, message) {
     //no spaces (if no punct)
     //no doubleposting
-    //no editing unless most recent
-    //three thumbs down on most recent -> deletes
     if(user == prevuser) { //doubleposting
         return -1;
     }
+    prevuser2 = prevuser;
     prevuser = user;
-    const spaceloc = message.indexOf(' ')
+    const spaceloc = message.indexOf(' ');
     if(spaceloc != -1) { //no spaces
-        //check punctuation
-        softpuncloc = containslocation(message, contpunct);
-        if(softpuncloc > spaceloc) {
-            //invalid
-            return -1;
-        }
+        return validsoftpunc(message, contpunct);
     }
-    if(containslocation(message, punctuation) != -1) {
+    let punctstat = onlycontains(message, punctuation);
+    console.log("does it contain punctuation?: %s", punctstat);
+    if(punctstat === 1) {
         return 0;
+    } else if(punctstat === -1) {
+        return -1;
     }
 }
 
-function containslocation(message, array) {
-    let res = -1;
-    let len = array.len;
-    while(len--) {
-        res = message.indexOf(array[len]);
-        if(res != -1) {
-            break;
+function validsoftpunc(message, array) {
+    let curr = -1;
+    let flag = true;
+    while(curr < message.length - 1) {
+        curr++;
+        if(flag) {
+            if(array.includes(message[curr])) {
+                continue;
+            }
+            if(message[curr] == ' ') {
+                flag = false;
+                continue;
+            }
+            return -1;
+        } else {
+            if(array.includes(message[curr])) {
+                return -1;
+            }
+            if(message[curr] == ' ') {
+                return -1;
+            }
         }
     }
-    return res;
+}
+
+function onlycontains(message, array) {
+    let curr = -1;
+    let flag;
+    while(curr < message.length - 1) {
+        curr++;
+        if(curr == 0) {
+            flag = array.includes(message[curr]);
+        }
+        if(flag != array.includes(message[curr])) {
+            return -1;
+        }
+    }
+    return Number(flag);
+}
+
+function construct(arr) {
+    let sent = "";
+    for (j in arr) {
+        i = arr[j];
+        if(onlycontains(i, punctuation) == 1) {
+            sent = sent + i;
+        } else if(i.indexOf(' ') != -1) {
+            sent = sent + i;
+        } else {
+            sent = sent + " " + i;
+        }
+    }
+    return sent;
 }
